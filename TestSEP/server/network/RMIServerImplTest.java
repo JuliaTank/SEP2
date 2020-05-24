1c028f7a7722a2a074eab0dd98ce60f862e17cbd
roxi2601 <roxi260111@gmail.com>
2020-05-24 22:05:13 +0200

    new

--- TestSEP/server/network/RMIServerImplTest.java
+++ TestSEP/server/network/RMIServerImplTest.java
@@ -54,9 +54,43 @@ class RMIServerImplTest {
     void addRecipeZero() throws IOException, SQLException {
         server.addRecipe(null,null,null,null,null,null);
         assertEquals(null,server.getRecipesByTitle(null));
+        assertEquals(null, server.getRecipesByIngredient(null));
+    }
+    //add recipe-one
+    @Test
+    void addRecipeOne() throws IOException, SQLException {
+        ArrayList<String> ingredients = new ArrayList<>();
+        server.addRecipe("kotlety","usmaz kotlety","kotki",ingredients,null,null);
+        assertEquals("kotlety",server.getRecipesByTitle("kotlety"));
+        assertEquals(ingredients.get(0),server.getRecipesByIngredient(ingredients.get(0)));
+    }
+    //add recipe-many
+    @Test
+    void addRecipeMany() throws IOException, SQLException {
+        ArrayList<String> ingredients1 = new ArrayList<>();
+        ArrayList<String> ingredients2 = new ArrayList<>();
+        server.addRecipe("kotlety","usmaz kotlety","kotki",ingredients1,null,null);
+        server.addRecipe("pierogi","ugotuj pierogi","pieski",ingredients2,null,null);
 
+        assertEquals("kotlety",server.getRecipesByTitle("kotlety"));
+        assertEquals(ingredients1.get(0),server.getRecipesByIngredient(ingredients1.get(0)));
+
+        assertEquals("pierogi",server.getRecipesByTitle("pierogi"));
+        assertEquals(ingredients2.get(0),server.getRecipesByIngredient(ingredients2.get(0)));
     }
 
+    //add recipe-boundary
+    @Test
+    void addRecipeBoundary()
+    {
+        //it's not necessary here, i think
+    }
+    //add recipe-exception
+    @Test
+    void addRecipeException()
+    {
+        assertThrows(IllegalArgumentException.class, ()->server.addRecipe("Fries","frytki","Julia",null,null,null))
+    }
     @Test
     void report() {
     }
